c8a68422df9264fdb258babeaff8727feb184ec0
honeydubey721 <honey.dubey721@gmail.com>
2026-02-17 22:45:05 +0530

    Time: 1 ms (25.29%), Space: 43 MB (27.9%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    //honey dubey
+    // Method to check if one string is a rotation of another
+    public boolean rotateString(String s, String goal) {
+        // Check if lengths of both strings are unequal
+        if (s.length() != goal.length()) {
+            // Return false if lengths don't match
+            return false;
+        }
+        // Concatenate the string with itself
+        String doubledS = s + s;
+        // Check if the goal is a substring of the concatenated string
+        return doubledS.contains(goal);
+    }
+}
