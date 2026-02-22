090e7e71734f201e62e97bb1598206b714ba5c8b
honeydubey721 <honey.dubey721@gmail.com>
2026-02-22 23:25:29 +0530

    Time: 0 ms (100%), Space: 42.6 MB (74.85%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,6 @@
+class Solution {
+    // Function to check if a number is a power of two
+    public boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & (n - 1)) == 0;  // Check if n is greater than 0 and has only one bit set
+    }
+}
