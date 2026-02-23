9f7dcf28b0832277e069385566c89febe7781c9b
honeydubey721 <honey.dubey721@gmail.com>
2026-02-24 00:21:22 +0530

    Time: 0 ms (100%), Space: 42.7 MB (80.29%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class MyStack {
+
+    Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q.add(x);
+        int size = q.size();
+        while (size-- > 1) {
+            q.add(q.remove());
+        }
+    }
+
+    public int pop() {
+        return q.remove();
+    }
+
+    public int top() {
+        return q.peek();
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
