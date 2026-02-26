08b734b02ace20ea5fc7e0ab58eebfd60a8e9398
honeydubey721 <honey.dubey721@gmail.com>
2026-02-27 00:04:32 +0530

    Time: 71 ms (33.8%), Space: 57.8 MB (58.92%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    
+    private int[] findNSE(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+                st.pop();
+            }
+            ans[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+        return ans;
+    }
+
+    private int[] findPSEE(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+                st.pop();
+            }
+            ans[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+        return ans;
+    }
+
+    public int sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+        int[] nse = findNSE(arr);
+        int[] psee = findPSEE(arr);
+
+        long mod = 1_000_000_007;
+        long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            long left = i - psee[i];
+            long right = nse[i] - i;
+            sum = (sum + (left * right % mod) * arr[i]) % mod;
+        }
+
+        return (int) sum;
+    }
+}
