fcf0837edede8d63c719fad70687d8047bee345e
honeydubey721 <honey.dubey721@gmail.com>
2026-02-27 00:53:10 +0530

    Time: 26 ms (39.49%), Space: 47.7 MB (55.05%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public String removeKdigits(String num, int k) {
+        Stack<Character> st = new Stack<>();
+        
+        for (char digit : num.toCharArray()) {
+            
+            while (!st.isEmpty() && k > 0 && st.peek() > digit) {
+                st.pop();
+                k--;
+            }
+            
+            st.push(digit);
+        }
+        
+        while (k > 0) {
+            st.pop();
+            k--;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty()) {
+            sb.append(st.pop());
+        }
+        
+        sb.reverse();
+        
+        while (sb.length() > 1 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+        
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
